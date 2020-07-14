07ad5284df39206756bad55d9bd5c4ff084c6058
alissin4444 <dev.alissonsantos@gmail.com>
2020-07-14 19:04:10 -0300

    inital commit

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,13 +4,7 @@ import { View, Text, TouchableOpacity } from "react-native";
 import { Container } from "./styles";
 
 const Home = ({ navigation }) => {
-  return (
-    <View justifyContent="center" alignItems="center" flex={1}>
-      <TouchableOpacity onPress={() => navigation.push("Fruits")}>
-        <Text>Visualize your fruits</Text>
-      </TouchableOpacity>
-    </View>
-  );
+  return <View justifyContent="center" alignItems="center" flex={1}></View>;
 };
 
 export default Home;
--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,14 @@
-
-import React from 'react'
-import { createStackNavigator } from '@react-navigation/stack';
+import React from "react";
+import { createStackNavigator } from "@react-navigation/stack";
 
 const Stack = createStackNavigator();
 
-import Home from './pages/Home';
-import Fruits from './pages/Fruits';
+import Home from "./pages/Home";
 
 export default function Routes() {
   return (
     <Stack.Navigator>
       <Stack.Screen name="Home" component={Home} />
-      <Stack.Screen name="Fruits" component={Fruits} />
     </Stack.Navigator>
   );
 }
