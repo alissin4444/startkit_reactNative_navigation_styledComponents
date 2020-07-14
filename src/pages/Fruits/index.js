fe58687b4ea388d4cbc3c8f55daf8176d7d13bc4
alissin4444 <dev.alissonsantos@gmail.com>
2020-07-14 18:49:50 -0300

    finalized

--- src/pages/Fruits/index.js
+++ src/pages/Fruits/index.js
@@ -1,28 +1,27 @@
-import React, { useState } from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import React, { useState } from "react";
+import { Text, TouchableOpacity } from "react-native";
 
-// import { Container } from './styles';
+import { Container } from "./styles";
 
 const Fruits = () => {
-
-  const [fruits, setFruits] = useState([])
+  const [fruits, setFruits] = useState([]);
 
   const addFruit = () => {
-    const fruitPosition = fruits.length + 1
-    const fruit = `Fruit ${fruitPosition}`
-    setFruits([...fruits, fruit])
-  }
+    const fruitPosition = fruits.length + 1;
+    const fruit = `Fruit ${fruitPosition}`;
+    setFruits([...fruits, fruit]);
+  };
 
   return (
-    <View justifyContent="center" alignItems="center" flex={1}>
-      {fruits.map(fruit => (
+    <Container>
+      {fruits.map((fruit) => (
         <Text key={fruit}>{fruit}</Text>
       ))}
       <TouchableOpacity onPress={addFruit}>
         <Text>Add a fruit</Text>
       </TouchableOpacity>
-    </View>
+    </Container>
   );
-}
+};
 
 export default Fruits;
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
+import React from "react";
+import { View, Text, TouchableOpacity } from "react-native";
 
-// import { Container } from './styles';
+import { Container } from "./styles";
 
 const Home = ({ navigation }) => {
   return (
     <View justifyContent="center" alignItems="center" flex={1}>
-      <TouchableOpacity onPress={() => navigation.push('Fruits')}>
+      <TouchableOpacity onPress={() => navigation.push("Fruits")}>
         <Text>Visualize your fruits</Text>
       </TouchableOpacity>
     </View>
   );
-}
+};
 
 export default Home;
